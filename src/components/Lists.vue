b8af4d02b5350ddea8e4e043161fd3e33a860dac
Pavel Yakupov <PVYAKUPOV@rofltech.ru>
2023-10-23 18:15:57 +0400

    fix

--- src/components/Lists.vue
+++ src/components/Lists.vue
@@ -2,37 +2,52 @@
 import { useColors } from "../store/colors";
 import Button from "./UI/Button.vue";
 import BoxItem from "./UI/BoxItem.vue";
-import { computed } from "vue";
+import { computed, watch } from "vue";
+import { shuffle as _shuffle } from "lodash-es";
+import { ref } from "vue";
 
 interface IListProps {
   list: string;
 }
 
+const index = ref(0);
 const props = defineProps<IListProps>();
 
 const colorsStore = useColors();
 
 function toggleShuffleItems(name: string) {
   colorsStore.shuffleItems(name);
+  getItems();
 }
 
-const shuffleData = computed(() => {
-  const allArray: Array<string> = [];
+function deleteItem(id: number, list: string) {
+  alert(id);
+  colorsStore.deleteItem(id, list);
+}
+
+const boxD = ref([]);
+getItems();
+
+function getItems() {
+  boxD.value = [];
+  let counter = 0;
   colorsStore.lists[props.list].data.forEach((item) => {
-    console.log(item);
     for (let i = 0; i < item.count; i++) {
-      allArray.push(item.color);
+      boxD.value.push({ id: counter, color: item.color, parentId: item.id });
+      counter++;
+    }
+    if (!colorsStore.lists[props.list].shuffle) {
+      boxD.value.push({ id: counter, break: true });
     }
   });
-  const shuffled = allArray
-    .map((value) => ({ value, sort: Math.random() }))
-    .sort((a, b) => a.sort - b.sort)
-    .map(({ value }) => value);
-  return shuffled;
-});
-
-function deleteItem(id: number, list: string) {
-  colorsStore.deleteItem(id, list);
+  if (colorsStore.lists[props.list].shuffle) {
+    boxD.value = boxD.value
+      .map((value) => ({ value, sort: Math.random() }))
+      .sort((a, b) => a.sort - b.sort)
+      .map(({ value }) => value);
+  } else {
+    return boxD.value;
+  }
 }
 </script>
 
@@ -41,16 +56,12 @@ function deleteItem(id: number, list: string) {
     <div style="align-self: flex-end">
       <Button @clickOnButton="toggleShuffleItems(list)" text="Перемешать" />
     </div>
-    {{ list }}
-    <div class="box-wrapper" v-for="box in colorsStore.lists[list].data">
+    <p>{{ list }}</p>
+    <div class="box-wrapper">
       <box-item
-        :key="box.id"
-        :shuffle="colorsStore.lists[list].shuffle"
-        :backgroundColor="box.color"
-        :shuffleData="shuffleData"
-        style="margin-left: 5px"
-        :count="box.count"
-        @delete-item="deleteItem(box.id, list)"
+        :boxesData="boxD"
+        @delete-item="deleteItem"
+        :shuffle="colorsStore.lists[props.list].shuffle"
       />
     </div>
   </div>
--- src/components/UI/BoxItem.vue
+++ src/components/UI/BoxItem.vue
@@ -1,27 +1,21 @@
 <template>
-  <template v-if="!shuffle">
-    <div
-      v-for=" in [...Array(count).keys()]"
-      class="box-item"
-      :style="{ backgroundColor: backgroundColor }"
-      @click="$emit('deleteItem')"
-    ></div>
-  </template>
-  <template v-else>
-    <div
-      v-for="i in shuffleData"
-      class="box-item"
-      :style="{ backgroundColor: i }"
-      @click="$emit('deleteItem')"
-    />
-  </template>
+  {{ boxesData.length }}
+  <TransitionGroup name="fade" tag="div" class="container">
+    <template v-for="item in boxesData" :key="item.id">
+      <div
+        class="box-item"
+        v-if="!item.break"
+        :style="{ backgroundColor: item.color }"
+        @click="$emit('deleteItem')"
+      ></div>
+      <div v-if="item.break"><br /></div>
+    </template>
+  </TransitionGroup>
 </template>
 
 <script lang="ts" setup>
 interface IColor {
-  backgroundColor: string;
-  count: number;
-  shuffleData?: Array<string>;
+  boxesData: Array<any>;
   shuffle: boolean;
 }
 defineProps<IColor>();
@@ -29,12 +23,40 @@ defineEmits(["deleteItem"]);
 </script>
 
 <style lang="scss">
+.container {
+  // display: flex;
+  // flex-direction: column;
+}
 .box-item {
   width: 16px;
   height: 16px;
   margin-left: 4px;
+  display: inline-block;
+  &:before {
+    content: "\a";
+    white-space: pre;
+  }
   &:hover {
     cursor: pointer;
   }
+}
+/* 1. declare transition */
+.fade-move,
+.fade-enter-active,
+.fade-leave-active {
+  transition: all 0.5s cubic-bezier(0.55, 0, 0.1, 1);
+}
+
+/* 2. declare enter from and leave to state */
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+  transform: scaleY(0.01) translate(30px, 0);
+}
+
+/* 3. ensure leaving items are taken out of layout flow so that moving
+      animations can be calculated correctly. */
+.fade-leave-active {
+  position: absolute;
 }
 </style>
